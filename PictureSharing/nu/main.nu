0a51a00603607a06b7449aa470b2b33d1b777706
Tim Burks <tim@neontology.com>
2008-04-21 23:12:34 -0700

    Converting PictureSharing to Nu.

--- PictureSharing/nu/main.nu
+++ PictureSharing/nu/main.nu
@@ -1,12 +1,68 @@
 (load "nu")      	;; essentials
 (load "cocoa")		;; wrapped frameworks
 (load "console")	;; interactive console
+(import Cocoa)
 
 ;; define the application delegate class
 (class PicSharingController
-     (imethod (void) applicationDidFinishLaunching: (id) sender is
-          (set $console ((NuConsoleWindowController alloc) init))
-          ($console toggleConsole:self)))
+     (- (void) applicationDidFinishLaunching: (id) sender is
+        (set $console ((NuConsoleWindowController alloc) init))
+        ($console toggleConsole:self))
+     
+     (- (void)connectionReceived:(id)notification is
+        (set incomingConnection ((notification userInfo) objectForKey:NSFileHandleNotificationFileHandleItem))
+        ((notification object) acceptConnectionInBackgroundAndNotify)
+        (incomingConnection writeData:((@imageView image) TIFFRepresentation));
+        (incomingConnection closeFile)
+        (set @numberOfDownloads (+ 1 @numberOfDownloads))
+        (@longerStatusText setStringValue:<<-END
+Click Stop to turn off Picture Sharing.
+Number of downloads this session: #{@numberOfDownloads}.END))
+     
+     ; This object is the delegate of the NSApplication instance so we can get notifications about various states.
+     ; Here, the NSApplication shared instance is asking if and when we should terminate. By listening for this
+     ; message, we can stop the service cleanly, and then indicate to the NSApplication instance that it's all right
+     ; to quit immediately.
+     (- (int) applicationShouldTerminate:(id)sender is
+        (if @netService (@netService stop))
+        NSTerminateNow)
+     
+     (- (void)awakeFromNib is
+        (set picture ((NSImage alloc) initWithContentsOfFile:"/Library/Desktop Pictures/Nature/Dew Drop.jpg"))
+        (if picture (@imageView setImage:picture))
+        ;; Set up a default name for the picture service. The user should change this, but it's not a big deal.
+        (@serviceNameField setStringValue:"Just another picture service"))
+     
+     (- (void)popupChangedPicture:(id)sender is
+        (set picture ((NSImage alloc) initWithContentsOfFile:
+                      (+ "/Library/Desktop Pictures/Nature/" ((sender selectedItem) title) ".jpg")))
+        (if picture (@imageView setImage:picture)))
+     
+     ;; This object is the delegate of its NSNetService. It should implement the NSNetServiceDelegateMethods that
+     ;; are relevant for publication (see NSNetServices.h).
+     (- (void)netServiceWillPublish:(id)sender is
+        (@toggleSharingButton setTitle:"Stop")
+        (@shortStatusText setStringValue:"Picture Sharing is on.")
+        (@longerStatusText setStringValue:"Click Stop to turn off Picture Sharing."))
+     
+     (- (void)netServiceDidStop:(id)sender is
+        (@toggleSharingButton setTitle:"Start")
+        (@shortStatusText setStringValue:"Picture Sharing is off.")
+        (@longerStatusText setStringValue:"Click Start to turn on Picture Sharing and allow other users to see a thumbnail of the picture below.")
+        ;; We'll need to release the NSNetService sending this, since we want to recreate it in sync with the socket
+        ;; at the other end. Since there's only the one NSNetService in this application, we can just release it.
+        (set @netService nil))
+     
+     (- (void)netService:(id)sender didNotPublish:(id)errorDict is
+        ;; Display some meaningful error message here, using the longerStatusText as the explanation.
+        (@toggleSharingButton setTitle:"Start")
+        (@shortStatusText setStringValue:"Picture Sharing is off.")        
+        (if (eq (errorDict objectForKey:NSNetServicesErrorCode) NSNetServicesCollisionError)
+            (then (@longerStatusText setStringValue:"A name collision occurred. A service is already running with that name someplace else.")
+                  (@serviceNameField setEnabled:YES))
+            (else (@longerStatusText setStringValue:"Some other unknown error occurred.")))        
+        (set @listeningSocket nil)
+        (set @netService nil)))
 
 ;; install the delegate and keep a reference to it since the application won't retain it.
 ((NSApplication sharedApplication) setDelegate:(set delegate ((PicSharingController alloc) init)))
--- PictureSharing/objc/PicSharingController.m
+++ PictureSharing/objc/PicSharingController.m
@@ -8,14 +8,10 @@
     IBOutlet id shortStatusText;
     IBOutlet id toggleSharingButton;
     IBOutlet id picturePopUpMenu;
-
     NSNetService * netService;
     NSFileHandle * listeningSocket;
-    
     int numberOfDownloads;
 }
-- (IBAction)toggleSharing:(id)sender;
-- (IBAction)popupChangedPicture:(id)sender;
 @end
 
 // imports required for socket initialization
@@ -25,12 +21,10 @@
 
 @implementation PicSharingController
 
-
 - (IBAction)toggleSharing:(id)sender {
 
     uint16_t chosenPort;
     if(!listeningSocket) {
-
         // Here, create the socket from traditional BSD socket calls, and then set up an NSFileHandle with
         //that to listen for incoming connections.
         int fdForListening;
@@ -45,35 +39,29 @@
             serverAddress.sin_family = AF_INET;
             serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
             serverAddress.sin_port = 0;
-
             // Allow the kernel to choose a random port number by passing in 0 for the port.
             if (bind(fdForListening, (struct sockaddr *)&serverAddress, namelen) < 0) {
                 close (fdForListening);
                 return;
             }
-
             // Find out what port number was chosen.
             if (getsockname(fdForListening, (struct sockaddr *)&serverAddress, &namelen) < 0) {
                 close(fdForListening);
                 return;
             }
-
             chosenPort = ntohs(serverAddress.sin_port);
-
             // Once we're here, we know bind must have returned, so we can start the listen
             if(listen(fdForListening, 1) == 0) {
                 listeningSocket = [[NSFileHandle alloc] initWithFileDescriptor:fdForListening closeOnDealloc:YES];
             }
         }
     }
-
     if(!netService) {
         // lazily instantiate the NSNetService object that will advertise on our behalf.  Passing in "" for the domain causes the service
         // to be registered in the default registration domain, which will currently always be "local"
         netService = [[NSNetService alloc] initWithDomain:@"" type:@"_wwdcpic._tcp." name:[serviceNameField stringValue] port:chosenPort];
         [netService setDelegate:self];
     }
-
     if(netService && listeningSocket) {
         if([[sender title] isEqual:@"Start"]) {
             numberOfDownloads = 0;
@@ -93,90 +81,4 @@
 }
 
 
-- (void)awakeFromNib {
-    NSImage * picture;
-    picture = [[NSImage alloc] initWithContentsOfFile:[NSString stringWithFormat:@"/Library/Desktop Pictures/Nature/Dew Drop.jpg"]];
-
-    if(picture) [imageView setImage:picture];
-
-    [picture release];
-    // Set up a default name for the picture service. The user should change this, but it's not a big deal.
-    [serviceNameField setStringValue:@"Just another picture service"];
-    [[NSApplication sharedApplication] setDelegate: self];
-}
-
-
-- (IBAction)popupChangedPicture:(id)sender {
-    NSImage * picture;
-    picture = [[NSImage alloc] initWithContentsOfFile:[NSString stringWithFormat:@"/Library/Desktop Pictures/Nature/%@.jpg", [[sender selectedItem] title]]];
-
-    if (picture) [imageView setImage:picture];
-
-    [picture release];
-}
-
-
-// This object is the delegate of the NSApplication instance so we can get notifications about various states.
-// Here, the NSApplication shared instance is asking if and when we should terminate. By listening for this
-// message, we can stop the service cleanly, and then indicate to the NSApplication instance that it's all right
-// to quit immediately.
-- (NSApplicationTerminateReply) applicationShouldTerminate:(NSApplication *)sender {
-
-    if(netService) [netService stop];
-        return NSTerminateNow;
-}
-
-
-// This object is the delegate of its NSNetService. It should implement the NSNetServiceDelegateMethods that
-// are relevant for publication (see NSNetServices.h).
-- (void)netServiceWillPublish:(NSNetService *)sender {
-    [toggleSharingButton setTitle:@"Stop"];
-    [shortStatusText setStringValue:@"Picture Sharing is on."];
-    [longerStatusText setStringValue:@"Click Stop to turn off Picture Sharing."];
-}
-
-
-- (void)netService:(NSNetService *)sender didNotPublish:(NSDictionary *)errorDict {
-    // Display some meaningful error message here, using the longerStatusText as the explanation.
-    [toggleSharingButton setTitle:@"Start"];
-    [shortStatusText setStringValue:@"Picture Sharing is off."];
-
-    if([[errorDict objectForKey:NSNetServicesErrorCode] intValue] == NSNetServicesCollisionError) {
-        [longerStatusText setStringValue:@"A name collision occurred. A service is already running with that name someplace else."];
-        [serviceNameField setEnabled:YES];
-    } else {
-        [longerStatusText setStringValue:@"Some other unknown error occurred."];
-    }
-
-    [listeningSocket release];
-    listeningSocket = nil;
-    [netService release];
-    netService = nil;
-}
-
-
-- (void)netServiceDidStop:(NSNetService *)sender {
-    [toggleSharingButton setTitle:@"Start"];
-    [shortStatusText setStringValue:@"Picture Sharing is off."];
-    [longerStatusText setStringValue:@"Click Start to turn on Picture Sharing and allow other users to see a thumbnail of the picture below."];
-
-    // We'll need to release the NSNetService sending this, since we want to recreate it in sync with the socket
-    // at the other end. Since there's only the one NSNetService in this application, we can just release it.
-    [netService release];
-    netService = nil;
-}
-
-
-// This object is also listening for notifications from its NSFileHandle. When an incoming connection is seen
-// by the listeningSocket object, we get the NSFileHandle representing the near end of the connection. We write
-// the thumbnail image to this NSFileHandle instance.
-- (void)connectionReceived:(NSNotification *)aNotification {
-    NSFileHandle * incomingConnection = [[aNotification userInfo] objectForKey:NSFileHandleNotificationFileHandleItem];
-    NSData * representationToSend = [[imageView image] TIFFRepresentation];
-    [[aNotification object] acceptConnectionInBackgroundAndNotify];
-    [incomingConnection writeData:representationToSend];
-    [incomingConnection closeFile];
-    numberOfDownloads++;
-    [longerStatusText setStringValue:[NSString stringWithFormat:@"Click Stop to turn off Picture Sharing.\nNumber of downloads this session: %d.", numberOfDownloads]];
-}
 @end
